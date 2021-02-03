078ff338a73fad8cc93178546d080325f3b7475d
S00180333 <36006393+S00180333@users.noreply.github.com>
2021-02-03 22:44:32 +0000

    Week1
    Week1 Lab User Interface+User Profiles

---
+++ CasualGamesLab/Assets/Scripts/CreateProfileController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateProfileController : MonoBehaviour
+{
+    public InputField UserName;
+    Color defaultcolor=Color.red;
+
+    public void SetProfileColor(Button ClickedButton)
+    {
+        if(ClickedButton!=null)
+        {
+            defaultcolor=ClickedButton.GetComponent<Image>().color;
+            Debug.Log(defaultcolor);
+        }
+    }
+    public void CreateProfile()
+    {
+        if(!string.IsNullOrEmpty(UserName.text))
+        {
+            string username = UserName.text;
+            UserProfile profile = new UserProfile()
+            {
+                Username = username,
+                CreatedOn = DateTime.UtcNow,
+                Color = defaultcolor
+            };
+            AddProfileToList(profile);
+            Debug.Log(profile.Username + ":" + profile.CreatedOn);
+        }
+    }
+    public void AddProfileToList(UserProfile profile)
+    {
+        if(profile!=null)
+        {
+            ProfileButtonController profileButton = 
+            Instantiate<ProfileButtonController>
+            (ProfileButtonPrefab, IsProfilesContent.transform);
+
+            profileButton.UpdateButton(profile);
+        }
+    }
+
+    public ProfileButtonController ProfileButtonPrefab;
+    public GameObject IsProfilesContent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CasualGamesLab/Assets/Scripts/ProfileButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileButtonController : MonoBehaviour
+{
+   
+    public Image background;
+    public Text usernametxt;
+    public void UpdateButton(UserProfile profile)
+    {
+        usernametxt.text = profile.Username;
+        background.color = profile.Color;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
